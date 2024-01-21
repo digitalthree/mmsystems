25aedd26412f7adb5be9aea2fb3ea7f7b4ee04f1
ekin04 <mirko.chiaverini@gmail.com>
2024-01-21 01:20:49 +0100

    Creazione pagina chi siamo senza effetti e con form non funzionante

--- src/app/(pages)/chisiamo/page.tsx
+++ src/app/(pages)/chisiamo/page.tsx
@@ -1,9 +1,152 @@
-import PageStructure from "@/app/components/PageStructure";
+"use client";
+import { globalStore } from "@/store/globalStore";
+import ChiSiamoForm from "@/app/components/form/chiSiamoForm";
+import Image from "next/image";
+import { URL } from "url";
+import clienti from "../../../../public/img/clienti/clienti.png";
 
 export default function Page() {
-    return(
-        <PageStructure>
-            <h1>Chi Siamo</h1>
-        </PageStructure>
-    )
+  const navbarBackgroundColor = globalStore(
+    (state: any) => state.navbarBackgroundColor
+  );
+  const loading = globalStore((state: any) => state.loading);
+  return (
+    <div
+      className="w-full min-h-[85vh] flex flex-col justify-center items-center"
+      style={{ backgroundColor: navbarBackgroundColor }}
+    >
+      <div className="w-full bg-[#E4E4E4] flex justify-center">
+        {" "}
+        {/* chi siamo robot */}
+        <div className="container flex flex-row  p-10 ">
+          <div className=" overflow-visible ">
+            <Image
+              className=" w-8/12 md:w-[636px]"
+              src="/img/chisiamo/robot/robot-sx.webp"
+              alt="Robot image"
+              width={636}
+              height={636}
+            />
+            <h1 className=" text-primary text-5xl mt-8 lg:mt-12 xl:mt-24 xl:ml-10">
+              chi siamo
+            </h1>
+            <div className=" flex flex-col lg:absolute mt-3">
+              <span className=" text-[#3f3f3f] text-lg xl:text-xl font-normal xl:ml-10">
+                Da oltre 10 anni MM Systems Srl fornisce all'Industria Impianti
+                nel <br />
+              </span>
+              <span className=" text-[#3f3f3f] text-lg xl:text-xl font-normal xl:ml-10">
+                campo dell'automazione e dell'impiantistica generale.
+              </span>
+            </div>
+          </div>
+          <div className=" hidden md:flex">
+            <Image
+              src="/img/chisiamo/robot/robot-dx.webp"
+              alt="Robot image"
+              width={1000}
+              height={1000}
+            />
+          </div>
+        </div>
+      </div>{" "}
+      {/* fine chi siamo robot */}
+      {/* Inizio sezione stats */}
+      <div className="container grid grid-cols-1 md:grid-cols-2 gap-7 p-10 my-10 text-center">
+        <div className="flex flex-col mb-5 md:text-left">
+          <span className=" xl:text-7xl text-5xl font-normal text-white">
+            + 1550 MQ
+          </span>
+          <span className=" text-primary text-2xl xl:text-3xl font-normal">
+            PRODUCTION AREA
+          </span>
+        </div>
+        <div className="flex flex-col mb-5  md:text-right">
+          <span className=" xl:text-7xl text-5xl font-normal text-white">
+            + 30%
+          </span>
+          <span className=" text-primary text-2xl xl:text-3xl font-normal">
+            DI FATTURATO ANNUO
+          </span>
+        </div>
+        <div className="flex flex-col mb-5 md:text-left">
+          <span className=" xl:text-7xl text-5xl font-normal text-white">
+            146 PROGETTI
+          </span>
+          <span className=" text-primary text-2xl xl:text-3xl font-normal">
+            COMPLETATI
+          </span>
+        </div>
+        <div className="flex flex-col mb-5 md:text-right justify-center">
+          <span className=" xl:text-7xl text-5xl font-normal text-white">
+            16 TOP
+          </span>
+          <span className=" text-primary text-2xl xl:text-3xl font-normal">
+            CLIENTS
+          </span>
+        </div>
+      </div>
+      {/* fine sezione stats */}
+      {/* inizio sezione innovazione ed esperienza */}
+      <div className=" w-full bg-[#e4e4e4] p-10 py-20">
+        <div className=" container flex flex-col justify-center mx-auto bg-[url(/img/chisiamo/brain.webp)] bg-no-repeat bg-scroll bg-contain bg-right">
+          <h3 className="font-normal text-4xl md:text-6xl text-[#3f3f3f] mb-12 md:pl-10">
+            innovazione <br /> ed esperienza
+          </h3>
+          <div className="w-full flex flex-col md:pl-10">
+            <span className=" text-primary text-lg font-semibold mb-3">
+              NULLA E' IMPOSSIBILE
+            </span>
+            <span className=" text-[#3f3f3f] font-normal text-lg mb-12 w-5/6 md:w-4/6 ">
+              realta' abruzzese consolidata nel panorama europeo dell'industria
+              di automazione robotica. La MM systems, dal 2007 ad oggi, grazie
+              al suo fondatore Miki Morleo, personalita' e professionista
+              particolarmente attivo nel settore della produzione di
+              apparecchiature elettriche ed elettromeccaniche; si distingue sul
+              territorio.
+            </span>
+          </div>
+
+          <div className="w-full flex flex-col justify-end items-end md:pr-10">
+            <span className=" text-primary text-lg font-semibold mb-3">
+              PERCHE'?
+            </span>
+            <span className=" text-[#3f3f3f] text-right text-lg font-normal mb-12 w-5/6 md:w-4/6">
+              Il costante interesse nei confronti della ricerca scientifica e
+              tecnologica. Infatti, MM Systems diviene portavoce e precursore di
+              concetti innovativi e al passo con i tempi, come l'industria 4.0.
+            </span>
+          </div>
+
+          <div className="w-full flex flex-col md:pl-10">
+            <span className=" text-primary text-lg font-semibold mb-3">
+              COSA SI INTENDE PER INDUSTRIA 4.0?
+            </span>
+            <span className=" text-[#3f3f3f] font-normal text-lg mb-12 w-5/6 md:w-4/6">
+              Massima espressione della quarta rivoluzione industriale, e i
+              cobot, robot collaborativi antropomorfi capaci di interagire con
+              l'uomo nello spazio di lavoro.
+            </span>
+          </div>
+        </div>
+        <div className="w-full">
+          <h3 className="font-normal text-4xl md:text-6xl text-[#3f3f3f] mb-5 xl:mb-12"></h3>
+        </div>
+        <div className="full flex flex-col justify-center items-center md:p-10 xl:pb-0">
+          <h3 className="font-normal text-4xl md:text-6xl text-[#3f3f3f]">
+            i nostri clienti
+          </h3>
+          <div className="divider w-full divider-neutral mx-auto"></div>
+          <Image
+            className="w-full"
+            src={clienti}
+            width={1000}
+            height={300}
+            alt="All client"
+          />
+        </div>
+      </div>
+      <ChiSiamoForm />
+    </div>
+  );
 }
---
+++ src/app/components/form/chiSiamoForm.tsx
@@ -0,0 +1,135 @@
+import Link from "next/link";
+import { RiMailSendLine } from "react-icons/ri";
+
+export interface chiSiamoFormProps {}
+
+const ChiSiamoForm: React.FC<chiSiamoFormProps> = ({}) => {
+  return (
+    <>
+      <div className=" w-full flex justify-center items-center bg-[#E4E4E4] py-4">
+        <form className="container flex flex-col md:flex-row justify-items-center md:gap-4 max-w-lg md:max-w-7xl mb-10">
+          <div className="w-full md:w-2/5 shadow-xl p-6 pb-0 md:pb-6 flex flex-col justify-center items-center bg-white">
+            <RiMailSendLine className="text-primary w-14 h-14 md:w-24 md:h-24" />
+            <span className="text-gray-900 text-2xl font-normal">
+              SELEZIONA
+            </span>
+            <span className="text-gray-900 text-2xl font-normal mb-8">
+              l'ufficio
+            </span>
+            <div className="w-full flex flex-row justify-center items-center">
+              <div
+                className="
+                bg-[#F6F6F6] justify-center mb-2 h-auto md:h-[340px] w-full flex md:flex-col md:w-3/4 bg-transparent"
+              >
+                <div className="join join-horizontal gap-1 md:gap-6 md:join-vertical rounded-none justify-center mx-auto">
+                  <input
+                    className="join-item btn bg-transparent btn-sm md:btn-lg font-normal max-w-52 text-stone-800 checked:bg-primary hover:bg-primary "
+                    type="radio"
+                    name="options"
+                    aria-label="Progettazione"
+                  />
+                  <input
+                    className="join-item btn bg-transparent btn-sm md:btn-lg font-normal max-w-52 text-stone-800 checked:bg-primary hover:bg-primary"
+                    type="radio"
+                    name="options"
+                    aria-label="Vendita"
+                  />
+                  <input
+                    className="join-item btn bg-transparent btn-sm md:btn-lg font-normal max-w-52 text-stone-800 checked:bg-primary hover:bg-primary  "
+                    type="radio"
+                    name="options"
+                    aria-label="Assistenza"
+                  />
+                  <input
+                    className="join-item btn bg-transparent btn-sm md:btn-lg font-normal max-w-52 text-stone-800 checked:bg-primary hover:bg-primary  "
+                    type="radio"
+                    name="options"
+                    aria-label="Marketing"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full md:w-3/5 p-10 bg-white shadow-xl flex flex-col justify-center ">
+            <label className="form-control w-full mb-4">
+              <div className="label">
+                <span className="label-text text-gray-900 font-normal">
+                  Nome:
+                </span>
+              </div>
+              <input
+                type="text"
+                className="input text-gray-900 bg-[#F6F6F6]"
+                required
+              />
+            </label>
+
+            <label className="form-control w-full mb-4">
+              <div className="label">
+                <span className="label-text text-gray-900 font-normal">
+                  Cognome:
+                </span>
+              </div>
+              <input
+                type="text"
+                className="input text-gray-900 w-full bg-[#F6F6F6]"
+                required
+              />
+            </label>
+            <label className="form-control w-full mb-4">
+              <div className="label">
+                <span className="label-text text-gray-900 font-normal">
+                  Email:
+                </span>
+              </div>
+              <input
+                type="email"
+                className="input text-gray-900 input-md w-full bg-[#F6F6F6]"
+                required
+              />
+            </label>
+
+            <label className="form-control w-full mb-4">
+              <div className="label">
+                <span className="label-text text-gray-900 font-normal">
+                  Richiesta Informazioni:
+                </span>
+              </div>
+              <textarea
+                className="textarea text-gray-900 h-24 bg-[#F6F6F6]"
+                required
+              ></textarea>
+            </label>
+
+            <div className="form-control">
+              <label className="label cursor-pointer justify-start">
+                <input
+                  type="checkbox"
+                  required
+                  className="checkbox border-gray-900 mr-2"
+                />
+                <span className="label-text text-gray-900 font-normal">
+                  Ho letto e accetto l'informativa sulla
+                  <Link
+                    href="/"
+                    className=" font-bold hover:underline underline-offset-4"
+                  >
+                    {" "}
+                    privacy
+                  </Link>{" "}
+                  {/* Inserire link alla privacy */}
+                </span>
+              </label>
+              <button className="btn bg-primary text-white active:bg-sky-400 hover:bg-sky-400 mt-6">
+                {" "}
+                Invia richiesta
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+export default ChiSiamoForm;
