054adf566aae4452a3f9207c3dc0b5ae24a4c2a4
ekin04 <mirko.chiaverini@gmail.com>
2024-01-23 05:47:45 +0100

    Resa funzionante la form, aggiunto un tema al config di tailwind per modificare il colore del selettore uffici

--- src/app/components/form/chiSiamoForm.tsx
+++ src/app/components/form/chiSiamoForm.tsx
@@ -1,13 +1,47 @@
 import Link from "next/link";
 import { RiMailSendLine } from "react-icons/ri";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 export interface chiSiamoFormProps {}
 
 const ChiSiamoForm: React.FC<chiSiamoFormProps> = ({}) => {
+  const form = useRef<HTMLFormElement>(null);
+  const SERVICE_ID = process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID as string;
+  const TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID as string;
+  const PUBLIC_KEY = process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY as string;
+  console.log("questa è la public key: " + PUBLIC_KEY);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        SERVICE_ID,
+        TEMPLATE_ID,
+        form.current as HTMLFormElement,
+        PUBLIC_KEY
+      )
+      .then((result) => {
+        console.log(result.text);
+        alert("Email Inviata");
+        (form.current as HTMLFormElement).reset();
+        return <></>;
+      })
+      .catch((error) => {
+        console.log(error.text);
+        alert("errore");
+      });
+  };
+
   return (
     <>
       <div className=" w-full flex justify-center items-center bg-[#E4E4E4] py-4">
-        <form className="container flex flex-col md:flex-row justify-items-center md:gap-4 max-w-lg md:max-w-7xl mb-10">
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="container flex flex-col md:flex-row justify-items-center md:gap-4 max-w-lg md:max-w-7xl mb-10"
+        >
           <div className="w-full md:w-2/5 shadow-xl p-6 pb-0 md:pb-6 flex flex-col justify-center items-center bg-white">
             <RiMailSendLine className="text-primary w-14 h-14 md:w-24 md:h-24" />
             <span className="text-gray-900 text-2xl font-normal">
@@ -23,27 +57,33 @@ const ChiSiamoForm: React.FC<chiSiamoFormProps> = ({}) => {
               >
                 <div className="join join-horizontal gap-1 md:gap-6 md:join-vertical rounded-none justify-center mx-auto">
                   <input
-                    className="join-item btn bg-transparent btn-sm md:btn-lg font-normal max-w-52 text-stone-800 checked:bg-primary hover:bg-primary "
+                    className="join-item btn bg-transparent btn-xs md:btn-lg font-normal max-w-52 text-stone-800 checked:bg-primary hover:bg-primary  "
                     type="radio"
-                    name="options"
+                    name="ufficio"
+                    value={"assistenza@asd.it"}
+                    aria-label="Assistenza"
+                    defaultChecked
+                    required
+                  />
+                  <input
+                    className="join-item btn bg-transparent btn-xs md:btn-lg font-normal max-w-52 text-stone-800 checked:bg-primary hover:bg-primary "
+                    type="radio"
+                    name="ufficio"
+                    value={"progettazione@asd.it"}
                     aria-label="Progettazione"
                   />
                   <input
-                    className="join-item btn bg-transparent btn-sm md:btn-lg font-normal max-w-52 text-stone-800 checked:bg-primary hover:bg-primary"
+                    className="join-item btn bg-transparent btn-xs md:btn-lg font-normal max-w-52 text-stone-800 checked:bg-primary hover:bg-primary"
                     type="radio"
-                    name="options"
+                    name="ufficio"
+                    value={"vendita@asd.it"}
                     aria-label="Vendita"
                   />
                   <input
-                    className="join-item btn bg-transparent btn-sm md:btn-lg font-normal max-w-52 text-stone-800 checked:bg-primary hover:bg-primary  "
+                    className="join-item btn bg-transparent btn-xs md:btn-lg font-normal max-w-52 text-stone-800 checked:bg-primary hover:bg-primary  "
                     type="radio"
-                    name="options"
-                    aria-label="Assistenza"
-                  />
-                  <input
-                    className="join-item btn bg-transparent btn-sm md:btn-lg font-normal max-w-52 text-stone-800 checked:bg-primary hover:bg-primary  "
-                    type="radio"
-                    name="options"
+                    name="ufficio"
+                    value={"marketing@asd.it"}
                     aria-label="Marketing"
                   />
                 </div>
@@ -61,6 +101,7 @@ const ChiSiamoForm: React.FC<chiSiamoFormProps> = ({}) => {
               <input
                 type="text"
                 className="input text-gray-900 bg-[#F6F6F6]"
+                name="nome"
                 required
               />
             </label>
@@ -74,6 +115,7 @@ const ChiSiamoForm: React.FC<chiSiamoFormProps> = ({}) => {
               <input
                 type="text"
                 className="input text-gray-900 w-full bg-[#F6F6F6]"
+                name="cognome"
                 required
               />
             </label>
@@ -86,6 +128,7 @@ const ChiSiamoForm: React.FC<chiSiamoFormProps> = ({}) => {
               <input
                 type="email"
                 className="input text-gray-900 input-md w-full bg-[#F6F6F6]"
+                name="email"
                 required
               />
             </label>
@@ -98,6 +141,7 @@ const ChiSiamoForm: React.FC<chiSiamoFormProps> = ({}) => {
               </div>
               <textarea
                 className="textarea text-gray-900 h-24 bg-[#F6F6F6]"
+                name="messaggio"
                 required
               ></textarea>
             </label>
@@ -113,16 +157,17 @@ const ChiSiamoForm: React.FC<chiSiamoFormProps> = ({}) => {
                   Ho letto e accetto l`informativa sulla
                   <Link
                     href="/"
-                    className=" font-bold hover:underline underline-offset-4"
+                    className=" ml-1 font-bold hover:underline underline-offset-4"
                   >
-                    {" "}
                     privacy
-                  </Link>{" "}
+                  </Link>
                   {/* Inserire link alla privacy */}
                 </span>
               </label>
-              <button className="btn bg-primary text-white active:bg-sky-400 hover:bg-sky-400 mt-6">
-                {" "}
+              <button
+                className="btn bg-primary text-white active:bg-sky-400 hover:bg-sky-400 mt-6"
+                type="submit"
+              >
                 Invia richiesta
               </button>
             </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#2A9DE9',
-        secondary: '#04203B'
-      }
+        primary: "#2A9DE9",
+        secondary: "#04203B",
+      },
     },
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#2a9de9",
+          "secondary": "#04203b",
+          "base-100": "#e4e4e4", 
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
-}
-export default config
+};
+export default config;
